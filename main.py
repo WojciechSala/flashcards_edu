bdfabf552ebeb148bcd7ccce8e0415842d2d2734
Wojciech Sala <wojteksala190@gmail.com>
2018-09-06 20:57:41 +0200

    2nd gui and starting logic

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 from tkinter import *
 
+def learning_form():
+	welcome.destroy()
+	answer.pack()
+
+
 root = Tk()
 root.title("Flashcards")
 
-Label(root, text="Question:").grid(row=0, column=0, sticky=W)
-Entry(root, width=50).grid(row=0, column=1, ipady=5)
 
-Label(root).grid(row=1)
+welcome = Frame(root)
 
-Label(root, text="Answer:").grid(row=2, column=0, sticky=W)
-Entry(root, width=50).grid(row=2, column=1, ipady=5)
+Label(welcome, text="Question:").grid(row=0, column=0, sticky=W)
+question_value = Entry(welcome, width=50).grid(row=0, column=1, ipady=5)
 
-Label(root).grid(row=3)
+Label(welcome).grid(row=1)
 
-Button(root, text="Start learning").grid(row=4)
+Label(welcome, text="Answer:").grid(row=2, column=0, sticky=W)
+answer_value = Entry(welcome, width=50).grid(row=2, column=1, ipady=5)
+
+Label(welcome).grid(row=3)
+
+start_learning = Button(welcome, text="Start learning", command=learning_form).grid(row=4)
+
+welcome.pack()
+
+
+answer = Frame(root)
+
+points_value = 0
+question = "sad"
+Label(answer, text=points_value).grid(row=0, column=0, sticky=NW)
+Label(answer).grid(row=1,)
+Label(answer, text=question_value).grid(row=2, column=2)
+
+Label(answer).grid(row=3)
+
+# randomly picked answer_value in def
+Button(answer).grid(row=4, column=0)
 
 
 root.mainloop()
